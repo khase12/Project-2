fb10c5211697b4f5960f33c67fdc4182da0e8aa7
Jaquez Upton <btkhase@Jaquezs-MacBook-Pro.local>
2019-01-10 23:06:02 -0500

    updated reviews controller

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -19,3 +19,40 @@
 // }
 // }
 // module.exports = partiesController
+
+const reviews = require ("../models/Products")
+const Reviews = require ("../models/Reviews")
+
+
+const reviewsController = {
+    index: (req,res) => {
+       Parties.find({}).then((reviews) => {
+            res.render('reviews/forms', {reviews})
+       })
+       
+    },
+    new: (req,res) => {
+        res.render("app/new")
+    },
+    create: (req,res) => {
+        console.log(req.body)
+        Parties.create({
+            parties: req.body.product,
+            description: req.body.description,
+            image: req.body.image,
+            postDate: req.body.postDate,
+          
+        }).then(parties => {
+            res.redirect("/")
+        })
+    },
+    show: (req,res) => {
+        const partiesId = req.params.id 
+        Parties.findById(partiesId).then((parties) => {
+            console.log(parties)
+            res.render("app/show", {parties})
+        })
+    },
+}
+
+module.exports = reviewsController
